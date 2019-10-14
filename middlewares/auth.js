282704f0af020e67dd6a0902dcef69b00d31a206
Jose Luis Represa <joseluis.represa@bbva.com>
2019-10-14 21:12:25 +0200

    remove solution

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,5 @@
 const admin = require('../lib/firebase-admin');
 
 module.exports = (req, res, next) => {
-  const sessionCookie = req.cookies.__session || '';
-
-  console.log('sessionCookie', sessionCookie)
-
-  admin.auth().verifySessionCookie(sessionCookie, true).then((decodedClaims) => {
-    next();
-  }).catch((error) => {
-    res.redirect('/');
-  });
+  // @TODO
 };

1d402407613631966a769a9c95ac96daa2cbaff0
Jose Luis Represa <joseluis.represa@bbva.com>
2019-10-18 17:51:37 +0200

    step-2

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const admin = require('../lib/firebase-admin');
+
+function uploadToFirebase(file, filename, mimetype) {
+    const storage = admin.storage();
+    const bucket = storage.bucket('gs://curso-nodejs-kairos.appspot.com');
+    const fileReference = bucket.file(filename);
+
+    return new Promise((resolve, reject) => {
+        file.pipe(
+            fileReference.createWriteStream({
+                metadata: {
+                    contentType: mimetype
+                }
+            })
+        )
+            .on('finish', resolve)
+            .on('error', reject);
+    });
+}
 
 // GET: List films
 router.get('/', (req, res, next) => {
